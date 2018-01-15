e8948a8bfe57efaefd616844b806e17ece7f36f2
Samuelson Brito <samuelsonma@hotmail.com>
2018-01-14 23:28:59 -0400

    criando interação

--- js/script.js
+++ js/script.js
@@ -1,1 +1,47 @@
-console.log('START');
+let notes = {data: []};
+
+let createNote = function(){
+	let input = document.querySelector('#form-add-note input[type="text"]');
+	let value = input.value;
+
+	console.log(value);
+	notes.data.push(value);
+
+	input.value = "";
+
+
+}
+
+document.addEventListener('DOMContentLoaded', function(event){
+
+	let formAddNotes = document.getElementById('form-add-note');
+
+	formAddNotes.addEventListener('submit',function (e){
+		e.preventDefault();
+		createNote();
+	});
+
+});
+
+document.addEventListener('click', function(e){
+	let notesTag = document.getElementById('notes');
+
+	if(e.target.parentElement === notesTag){
+		if(confirm('remover esta nota?')){
+
+			let listOfNotes = document.querySelectorAll('#notes li');
+
+			listOfNotes.forEach(function(item, index){
+
+				if(e.target === item){
+					notes.data.splice(index,1);
+				}
+
+			});
+
+			console.log('ok');
+		}
+	}
+
+
+});
